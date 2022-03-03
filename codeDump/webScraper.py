5bffae79e0fbc15c0b355edf0e928dbd94f51601
Mackenzie Marriott <mackenziemarriott@Mackenzies-MBP.broadband>
2022-03-03 00:52:56 +0000

    webScraperProgress
    webscraper changes made:
    * altered old code to find data from every table on a given page
    *need to be able to split each table up and also find out how many columns there are per table
    * only works for first
    * adding stagecoach support later

--- codeDump/webScraper.py
+++ codeDump/webScraper.py
@@ -7,12 +7,11 @@ r = requests.get(url)
 
 soup = BeautifulSoup(r.content, 'html5lib')
 
-quotes=[]
+allData=[]
+i = 0
+divContainer = soup.findAll('td')
+for td in divContainer:
+    allData.append(divContainer[i].text)
+    i+=1
 
-table = soup.find('table')
-
-tableData = table.findAll('td')
-for td in tableData:
-    quotes.append(td.text)
-
-print(quotes)
+#table = soup.find('table')
