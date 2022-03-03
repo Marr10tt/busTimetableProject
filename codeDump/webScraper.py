d3dc52348470de740162dcbbf25a50dd6e1a61c1
Mackenzie Marriott <mackenziemarriott@Mackenzies-MBP.broadband>
2022-03-03 15:05:45 +0000

    filePaths
    changed code so that new text files are saved to the correct path, as well as closing the file after use
    conducted a test to accomodate this

--- codeDump/webScraper.py
+++ codeDump/webScraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import csv
 import requests
+import os
 
 def webScrape():
     print("get urls from bustimes.org or the first bus website - other urls may not be compatible")
@@ -8,13 +9,16 @@ def webScrape():
     urlRequest = requests.get(url)   
     soup = BeautifulSoup(urlRequest.content, 'html5lib')
 
-    fileName = input("Input File Name:")
+    fileName = input("Input File Name:")+'.txt'
+    pathName = ('/Users/mackenziemarriott/Documents/GitHub/busTimetableProject/codeDump/testFiles/txtTests')
+    completeFileName = os.path.join(pathName, fileName)
 
     tdValue = 0
     tdContainer = soup.findAll('td')
     for td in tdContainer:
-        with open(fileName+'.txt', 'a') as openFile:
+        with open(completeFileName, 'a') as openFile:
             openFile.write((tdContainer[tdValue].text+'\n'))
+        openFile.close()
         tdValue+=1
 
 webScrape()
