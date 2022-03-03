744a592c29dbce4314b4a9a2fcefec7ecbe9ad93
Mackenzie Marriott <mackenziemarriott@Mackenzies-MBP.broadband>
2022-03-03 01:26:41 +0000

    webScraperTest
    tested for use on different website - partial success - data is drawn out successfully, just in a different format, however more bus times are available here

--- codeDump/webScraper.py
+++ codeDump/webScraper.py
@@ -2,16 +2,19 @@ from bs4 import BeautifulSoup
 import csv
 import requests
 
-url = "https://www.firstbus.co.uk/doncaster/plan-journey/timetables/?day=1&source_id=2&service=72%2F72x%2F73%2F73x&routeid=23935110&operator=34&source=sp"
+url = "https://bustimes.org/services/22-worksop-doncaster-3"
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content, 'html5lib')
 
-i = 0
-divContainer = soup.findAll('td')
-for td in divContainer:
-    with open('rawData.txt', 'a') as f:
-        f.write((divContainer[i].text+'\n'))
-    i+=1
+def firstBusWebScrape():
+    i = 0
+    divContainer = soup.findAll('td')
+    for td in divContainer:
+        with open('test.txt', 'a') as f:
+            f.write((divContainer[i].text+'\n'))
+        i+=1
+
+firstBusWebScrape()
 
 #table = soup.find('table')
