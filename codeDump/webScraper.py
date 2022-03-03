3d793e6ba9517add919dc94c41761c6cf16670cb
Mackenzie Marriott <mackenziemarriott@Mackenzies-MBP.broadband>
2022-03-03 14:54:37 +0000

    folders
    Added folders and moved text files to store results from web scraping

--- codeDump/webScraper.py
+++ codeDump/webScraper.py
@@ -5,16 +5,16 @@ import requests
 def webScrape():
     print("get urls from bustimes.org or the first bus website - other urls may not be compatible")
     url = input("Input exact URL to obtain timetable data:")
-    r = requests.get(url)   
-    soup = BeautifulSoup(r.content, 'html5lib')
+    urlRequest = requests.get(url)   
+    soup = BeautifulSoup(urlRequest.content, 'html5lib')
 
     fileName = input("Input File Name:")
 
-    i = 0
-    divContainer = soup.findAll('td')
-    for td in divContainer:
-        with open(fileName+'.txt', 'a') as f:
-            f.write((divContainer[i].text+'\n'))
-        i+=1
+    tdValue = 0
+    tdContainer = soup.findAll('td')
+    for td in tdContainer:
+        with open(fileName+'.txt', 'a') as openFile:
+            openFile.write((tdContainer[tdValue].text+'\n'))
+        tdValue+=1
 
 webScrape()
