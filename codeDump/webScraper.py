07420ad4e862910a65db10a6b59391dc10c3eb2b
Mackenzie Marriott <mackenziemarriott@Mackenzies-MBP.broadband>
2022-03-03 14:43:17 +0000

    web scrape function
    Wrapped entire code into function
    lets user specify file name

--- codeDump/webScraper.py
+++ codeDump/webScraper.py
@@ -2,15 +2,18 @@ from bs4 import BeautifulSoup
 import csv
 import requests
 
-url = "https://bustimes.org/services/22-worksop-doncaster-3"
-r = requests.get(url)
-
-soup = BeautifulSoup(r.content, 'html5lib')
-
 def webScrape():
+    url = "https://bustimes.org/services/22-worksop-doncaster-3"
+    r = requests.get(url)   
+    soup = BeautifulSoup(r.content, 'html5lib')
+
+    fileName = input("Input File Name:")
+
     i = 0
     divContainer = soup.findAll('td')
     for td in divContainer:
-        with open('test.txt', 'a') as f:
+        with open(fileName+'.txt', 'a') as f:
             f.write((divContainer[i].text+'\n'))
         i+=1
+
+webScrape()
