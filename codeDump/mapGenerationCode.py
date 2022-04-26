6ad205fc254fd9863e4c010076b8548d6b0af4d1
Mackenzie Marriott <Mackenziejmarriott@gmail.com>
2022-04-26 14:10:36 +0100

    write up progress
    well into evaluation section

--- codeDump/mapGenerationCode.py
+++ codeDump/mapGenerationCode.py
@@ -18,7 +18,6 @@ def mapSettings(width, height, location):
 
 #generates the actual map selecting source, location, and places the map
 def generate(pageName):
-
     map_widget = TkinterMapView(pageName, width=windowWidth, height=windowHeight, corner_radius=0)
     map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)  # google normal
     map_widget.set_address(Location, marker=True)
